09a84c097212e4968122b6851f1946ba5726ed17
Jaga <Jaga@JP>
2025-06-26 22:00:06 +0530

    pushed to repoproject

--- RepoProject/src/test/java/contactsTests/CreateContact.java
+++ RepoProject/src/test/java/contactsTests/CreateContact.java
@@ -5,6 +5,7 @@ public class CreateContact {
 	public static void main(String[] args) {
 		System.out.println("Created by Rasikala and sent for review");
 		System.out.println("Reviewed by surendra and merged");
+		System.out.println("Contactlist created successfully");
 	}
 
 }
